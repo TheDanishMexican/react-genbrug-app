667fb7d968c4bd1acc70743174befb2df88562d4
TheDanishMexican <danieljchristjansen@gmail.com>
2023-10-31 21:48:12 +0100

    added href to fb and insta logos

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -10,18 +10,22 @@ export default function Footer({ getTabIndex }) {
               PRAKTISKE INFO
             </li>
             <li className="navBar-footer-li">
-              <img
-                className="footer-img"
-                src="https://live.staticflickr.com/65535/49813049796_0030ca3f16_z.jpg"
-                alt="no pic found"
-              />
+              <a href="https://www.instagram.com/rodekorsgenbrug/?hl=en">
+                <img
+                  className="footer-img"
+                  src="https://live.staticflickr.com/65535/49813049796_0030ca3f16_z.jpg"
+                  alt="no pic found"
+                />
+              </a>
             </li>
             <li className="navBar-footer-li">
-              <img
-                className="footer-img"
-                src="https://upload.wikimedia.org/wikipedia/commons/c/c2/Facebook_icon_192.png"
-                alt="no pic found"
-              />
+              <a href="https://www.facebook.com/Rodekorsgenbrug/?locale=da_DK">
+                <img
+                  className="footer-img"
+                  src="https://upload.wikimedia.org/wikipedia/commons/c/c2/Facebook_icon_192.png"
+                  alt="no pic found"
+                />
+              </a>
             </li>
           </ul>
         </nav>
